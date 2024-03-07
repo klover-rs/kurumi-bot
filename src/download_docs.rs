8af9353390e56a19c802af08bd8da47b8bf43fa4
Asm-Rosie <joelkerbusch@gmail.com>
2024-03-07 22:40:15 +0100

    use github repo key

--- src/download_docs.rs
+++ src/download_docs.rs
@@ -14,14 +14,30 @@ fn get_github_token() -> String {
         None => panic!("GITHUB_TOKEN key not found"),
     };
 
-    println!("GITHUB_TOKEN: {}", discord_token);
     discord_token.to_string()
+}
+
+fn get_repo() -> String {
+    let contents = fs::read_to_string("Secrets.toml").unwrap();
+
+    let data: toml::Value = contents.parse().unwrap();
+
+    let repo = match data.get("GITHUB_REPO") {
+        Some(repo) => match repo.as_str() {
+            Some(repo_str) => repo_str,
+            None => panic!("GITHUB_REPO value is not a string"),
+        },
+        None => panic!("GITHUB_REPO key not found"),
+    };
+
+    println!("GITHUB_REPO: {}", repo);
+    repo.to_string()
 }
 
 pub async fn fetch_docs(which: &str) -> Result<String, Box<dyn std::error::Error>> {
     let url = format!(
-        "https://raw.githubusercontent.com/Asm-Rosie/adhd-helper-bot/main/docs/{}",
-        which
+        "https://raw.githubusercontent.com/{}/main/docs/{}",
+        get_repo() , which
     );
 
     let client = reqwest::Client::new();
