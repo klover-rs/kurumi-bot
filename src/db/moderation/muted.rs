97cceb91bf8cf669b163b7f3882c160702b48491
Asm-Rosie <joelkerbusch@gmail.com>
2024-03-15 09:42:10 +0100

    new database layout

--- src/db/moderation/muted.rs
+++ src/db/moderation/muted.rs
@@ -1,4 +1,4 @@
-use rusqlite::{params, Result as RusqliteResult};
+/*use rusqlite::{params, Result as RusqliteResult};
 use r2d2_sqlite::SqliteConnectionManager;
 use r2d2::Pool;
 use r2d2::Error as R2d2Error;
@@ -84,4 +84,103 @@ impl Database {
         Ok(())
     }
 
+}*/
+
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
+use tokio_postgres::{NoTls, Error, Row};
+
+pub struct Database {
+    pool: Pool<PostgresConnectionManager<NoTls>>,
+}
+
+
+#[derive(Debug)]
+pub struct Muted {
+    pub uid: i64,
+    pub guild_id: i64,
+    pub reason: String,
+    pub roles: String,
+    pub duration: i64,
+}
+
+impl Database {
+    // Initialize the connection pool
+    pub async fn new() -> Result<Self, Error> {
+        let manager = PostgresConnectionManager::new_from_stringlike(
+            "host=localhost user=postgres password=7522",
+            NoTls,
+        ).expect("Failed to create connection manager");
+
+        let pool = Pool::builder().build(manager).await.expect("Failed to build pool");
+        Ok(Database { pool })
+    }
+
+    pub async fn create_table(&self) -> Result<(), Box<dyn std::error::Error>> { // Note the change in the return type here
+        let conn = self.pool.get().await?;
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS muted (
+                uid BIGINT PRIMARY KEY,
+                guild_id BIGINT,
+                reason TEXT,
+                roles TEXT,
+                duration BIGINT
+            )",
+            &[],
+        ).await?;
+        Ok(())
+    }
+
+    pub async fn insert(&self, uid: i64, guild_id: i64, reason: &str, roles: Vec<u64>, duration: i64) -> Result<(), Box<dyn std::error::Error>> {
+        let mut conn = self.pool.get().await?;
+
+        let roles_str = roles.iter().map(|&role| role.to_string()).collect::<Vec<String>>().join(",");
+
+        let trans = conn.transaction().await?;
+
+        trans.execute(
+            "INSERT INTO muted (uid, guild_id, reason, roles, duration) VALUES ($1, $2, $3, $4, $5)",
+            &[&uid, &guild_id, &reason, &roles_str, &duration],
+        ).await?;
+
+        trans.commit().await?;
+        Ok(())
+    }
+
+    pub async fn read_muted(&self) -> Result<Vec<Muted>, Box<dyn std::error::Error>> {
+        let mut muted_records = Vec::new();
+        let connection = self.pool.get().await?;
+
+        let statement = connection.prepare("SELECT * FROM muted").await?;
+
+        let rows = connection.query(&statement, &[]).await?;
+
+        for row in rows {
+            muted_records.push(parse_muted_record(row)?);
+        }
+
+        Ok(muted_records)
+    }
+
+    pub async fn delete(&self, uid: i64) -> Result<(), Box<dyn std::error::Error>> {
+        let mut conn = self.pool.get().await?;
+
+        let trans = conn.transaction().await?;
+
+        trans.execute("DELETE FROM muted WHERE uid = $1", &[&uid]).await?;
+
+        trans.commit().await?;
+        Ok(())
+    }
+
+}
+
+fn parse_muted_record(row: Row) -> Result<Muted, Error> {
+    Ok(Muted {
+        uid: row.try_get(0)?,
+        guild_id: row.try_get(1)?,
+        reason: row.try_get(2)?,
+        roles: row.try_get(3)?,
+        duration: row.try_get(4)?,
+    })
 }
--- src/db/timer.rs
+++ src/db/timer.rs
@@ -1,4 +1,4 @@
-use rusqlite::{params, Result as RusqliteResult};
+/*use rusqlite::{params, Result as RusqliteResult};
 use r2d2_sqlite::SqliteConnectionManager;
 use r2d2::Pool;
 use r2d2::Error as R2d2Error;
@@ -105,4 +105,120 @@ impl Database {
 
         Ok(())
     }
+}*/
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
+use tokio_postgres::{NoTls, Error, Row};
+
+pub struct Database {
+    pool: Pool<PostgresConnectionManager<NoTls>>,
+}
+
+
+#[derive(Debug)]
+pub struct Timer {
+    pub id: i32,
+    pub uid: i64,
+    pub description: String,
+    pub duration: i64,
+    pub dm_channel: i64,
+}
+
+impl Database {
+    // Initialize the connection pool
+    pub async fn new() -> Result<Self, Error> {
+        let manager = PostgresConnectionManager::new_from_stringlike(
+            "host=localhost user=postgres password=7522",
+            NoTls,
+        ).expect("Failed to create connection manager");
+
+        let pool = Pool::builder().build(manager).await.expect("Failed to build pool");
+        Ok(Database { pool })
+    }
+
+    pub async fn create_table(&self) -> Result<(), Error> { // Note the change in the return type here
+        let conn = self.pool.get().await.unwrap();
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS timer (
+                id INTEGER PRIMARY KEY,
+                uid BIGINT,
+                description TEXT,
+                duration INTEGER,
+                dm_channel BIGINT
+            )",
+            &[],
+        ).await?;
+        Ok(())
+    }
+
+    pub async fn insert(&self, uid: i64, description: &str, time: i64, dm_channel: i64) -> Result<(), Error> {
+        let mut conn = self.pool.get().await.unwrap();
+
+        let trans = conn.transaction().await?;
+
+        trans.execute(
+            "INSERT OR REPLACE INTO timer (uid, description, duration, dm_channel, bot_message_id) VALUES ($1, $2, $3, $4)",
+            &[&uid, &description, &time, &dm_channel],
+        ).await?;
+
+        trans.commit().await?;
+
+        Ok(())
+    
+    }
+
+    pub async fn read(&self) -> Result<Vec<Timer>, Error> {
+        let mut timer_records = Vec::new();
+
+        let conn = self.pool.get().await.unwrap();
+
+        let stmt = conn.prepare("SELECT * FROM timer").await?;
+
+        let rows = conn.query(&stmt, &[]).await?;
+
+        for row in rows {
+            timer_records.push(parse_timer_record(row)?);
+        }
+
+        Ok(timer_records)
+    }
+
+    pub async fn read_by_uid(&self, uid: i64) -> Result<Vec<Timer>, Error> {
+        let mut timer_records = Vec::new();
+
+        let conn = self.pool.get().await.unwrap();
+
+        let stmt = conn.prepare("SELECT * FROM timer WHERE uid = $1").await?;
+
+        let rows = conn.query(&stmt, &[&uid]).await?;
+
+        for row in rows {
+            timer_records.push(parse_timer_record(row)?);
+        }
+
+        Ok(timer_records)
+
+    }
+
+    pub async fn delete_by_id(&self, id: i32) -> Result<(), Error> {
+        let mut conn = self.pool.get().await.unwrap();
+
+        let trans = conn.transaction().await?;
+
+        trans.execute("DELETE FROM timer WHERE id = $1", &[&id]).await?;
+
+        trans.commit().await?;
+        Ok(())
+    }
+}   
+
+fn parse_timer_record(row: Row) -> Result<Timer, Error> {
+    Ok(Timer {
+        id: row.try_get(0)?,
+        uid: row.try_get(1)?,
+        description: row.try_get(2)?,
+        duration: row.try_get(3)?,
+        dm_channel: row.try_get(4)?,
+    })
+
 }
