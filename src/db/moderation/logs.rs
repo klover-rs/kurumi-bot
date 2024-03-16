5bf65860a848ff31e3b652b287c1e8d489eb5553
Asm-Rosie <joelkerbusch@gmail.com>
2024-03-16 01:10:25 +0100

    transition to new database layout

--- src/db/moderation/logs.rs
+++ src/db/moderation/logs.rs
@@ -1,95 +1,110 @@
-use rusqlite::{params, Result as RusqliteResult};
-use r2d2_sqlite::SqliteConnectionManager;
-use r2d2::Pool;
-use r2d2::Error as R2d2Error;
-use std::path::Path;
-use std::sync::Mutex;
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
+use tokio_postgres::{NoTls, Error, Row};
 
 pub struct Database {
-	pool: Mutex<Pool<SqliteConnectionManager>>,
+    pool: Pool<PostgresConnectionManager<NoTls>>,
+}
+
+pub struct MsgLogs {
+    pub msg_id: i64,
+    pub guild_id: i64,
+    pub channel_id: i64,
+    pub author_id: i64,
+    pub content: String,
 }
 
 impl Database {
-    pub fn new(database_path: &str) -> Result<Self, r2d2::Error> {
-        let manager = SqliteConnectionManager::file(Path::new(database_path));
-        let pool = Pool::new(manager).map_err(R2d2Error::from)?;
-        Ok(Database { pool: Mutex::new(pool) })
+    pub async fn new() -> Result<Self, Error> {
+        let manager = PostgresConnectionManager::new_from_stringlike(
+            "host=localhost user=postgres password=7522",
+            NoTls,
+        ).expect("Failed to create connection manager");
+
+        let pool = Pool::builder().build(manager).await.expect("Failed to build pool");
+        Ok(Database { pool })
     }
 
-    pub fn create_table_logs(&self) -> RusqliteResult<()> {
-        let connection = self.pool.lock().unwrap().get().unwrap();
-
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS logs (
-                msg_id INTEGER PRIMARY KEY,
-                guild_id INTEGER,
-                channel_id INTEGER,
-                author_id INTEGER,
+    pub async fn create_table_msg_logs(&self) -> Result<(), Error> {
+        let conn = self.pool.get().await.unwrap();
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS msg_logs (
+                msg_id BIGINT PRIMARY KEY,
+                guild_id BIGINT,
+                channel_id BIGINT,
+                author_id BIGINT,
                 content TEXT
-            )", [],
-            
-        )?;
+            )", &[],
+        ).await?;
 
         println!("Logs table created");
         Ok(())
+
     }
 
-    pub fn insert_log(&self, msg_id: u64, guild_id: u64, channel_id: u64, author_id: u64, content: &str, attachments: Option<String>) -> RusqliteResult<()> {
-        let mut connection = self.pool.lock().unwrap().get().unwrap();
+    pub async fn insert_msg_logs(&self, msg_id: i64, guild_id: i64, channel_id: i64, author_id: i64, content: &str, attachments: Option<String>) -> Result<(), Error> {
+        let mut conn = self.pool.get().await.unwrap();
 
-        let trans = connection.transaction()?;
-        // Insert the new log entry
+        let trans = conn.transaction().await?;
+
         trans.execute(
-            "INSERT INTO logs (msg_id, guild_id, channel_id, author_id, content) VALUES (?1, ?2, ?3, ?4, ?5)",
-            params![msg_id, guild_id, channel_id, author_id, content],
-        )?;
+            "INSERT INTO msg_logs (msg_id, guild_id, channel_id, author_id, content) VALUES ($1, $2, $3, $4, $5)",
+            &[&msg_id, &guild_id, &channel_id, &author_id, &content],
+        ).await?;
 
-        // Check the number of rows in the table
-        let row_count: u64 = trans.query_row("SELECT COUNT(*) FROM logs", [], |row| row.get(0)).unwrap();
+        
+        let row_count: i64 = trans.query_one("SELECT COUNT(*) FROM msg_logs", &[]).await?.get(0);
 
         if row_count > 1000 {
             trans.execute(
                 "DELETE FROM logs WHERE msg_id = (SELECT msg_id FROM logs ORDER BY msg_id ASC LIMIT 1)",
-                [],
-            )?;
+                &[],
+            ).await?;
         }
 
-        trans.commit()?;
+        trans.commit().await?;
 
         Ok(())
     }
 
-    pub fn update_log_content(&self, msg_id: u64, new_content: &str) -> RusqliteResult<()> {
-        let mut connection = self.pool.lock().unwrap().get().unwrap();
+    pub async fn read_logs_by_id(&self, msg_id: i64) -> Result<Vec<MsgLogs>, Error> {
+        let conn = self.pool.get().await.unwrap();
 
-        let trans = connection.transaction()?;
+        let statement = conn.prepare("SELECT * FROM msg_logs WHERE msg_id = $1").await?;
+
+        let rows = conn.query(&statement, &[&msg_id]).await?;
+
+        let mut msg_logs = Vec::new();
+
+        for row in rows {
+            msg_logs.push(parse_msg_logs_record(row)?);
+        }
+
+        Ok(msg_logs)
+    }
+
+    pub async fn update_logs_by_id(&self, msg_id: i64, new_content: &str) -> Result<(), Error> {
+        let mut conn = self.pool.get().await.unwrap();
+        
+        let trans = conn.transaction().await?;
 
         trans.execute(
-            "UPDATE logs SET content = ?1 WHERE msg_id = ?2",
-            params![new_content, msg_id],
-        )?;
+            "UPDATE msg_logs SET content = $1 WHERE msg_id = $2",
+            &[&new_content, &msg_id],
+        ).await?;
 
-        trans.commit()?;
+        trans.commit().await?;
 
         Ok(())
     }
+}
 
-    pub fn read_logs_by_id(&self, msg_id: u64) -> RusqliteResult<Vec<(u64, u64, u64, u64, String)>> {
-        let connection = self.pool.lock().unwrap().get().unwrap();
-
-        let mut statement = connection.prepare("SELECT * FROM logs WHERE msg_id = ?1")?;
-
-        let rows = statement.query_map(params![msg_id], |row| {
-            Ok((row.get(0)?, row.get(1)?, row.get(2)?, row.get(3)?, row.get(4)?))
-        })?;
-
-        let data: Result<Vec<_>, _> = rows.collect();
-
-        match data {
-            Ok(data) => Ok(data),
-            Err(rusqlite::Error::QueryReturnedNoRows) => Ok(Vec::new()),
-            Err(err) => Err(err),
-        }
-    }
-
+fn parse_msg_logs_record(row: Row) -> Result<MsgLogs, Error> {
+    Ok(MsgLogs {
+        msg_id: row.try_get(0)?,
+        guild_id: row.try_get(1)?,
+        channel_id: row.try_get(2)?,
+        author_id: row.try_get(3)?,
+        content: row.try_get(4)?
+    })
 }
