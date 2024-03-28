b2d0bbb96a95ab599004c82e12baf5fdcd8057fa
Imo-Br <imobr106@gmail.com>
2024-03-28 16:55:20 +0000

    Added a hug command that uses the neko api wrapper

---
+++ src/commands/user/hug.rs
@@ -0,0 +1,38 @@
+use std::vec;
+
+use crate::{Context, Error};
+
+use core::result::Result::Ok;
+
+use poise::{serenity_prelude as serenity, serenity_prelude::CreateMessage};
+
+use serenity::{
+    all::colours,
+    builder::{CreateEmbed, CreateEmbedAuthor},
+};
+
+#[poise::command(prefix_command)]
+pub async fn hug(ctx: Context<'_>, user: serenity::User) -> Result<(), Error> {
+    let image_url = get_hug_image().await.expect("Failed to get hug image");
+    let avatar_url = ctx.author().avatar_url().unwrap();
+
+    let embed = CreateEmbed::new()
+        .image(image_url)
+        .author(
+            CreateEmbedAuthor::new(format!(
+                "{} gave a warm hug to {} ",
+                ctx.author().name,
+                &user.name
+            ))
+            .icon_url(avatar_url),
+        )
+        .colour(colours::roles::BLUE);
+    let builder = CreateMessage::new().embed(embed);
+    ctx.channel_id().send_message(&ctx.http(), builder).await?;
+    Ok(())
+}
+
+async fn get_hug_image() -> Result<String, Box<dyn std::error::Error>> {
+    let url: String = nekoslife::get(nekoslife::SfwCategory::Hug).await?;
+    Ok(url)
+}
--- src/commands/user/mod.rs
+++ src/commands/user/mod.rs
@@ -1,2 +1,3 @@
+pub mod hug;
 pub mod image_to_ascii;
 pub mod snipe;
